21c0c074e4090ab9da4f986215dbd070671552dc
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2025-01-28 21:06:42 -0500

    TransactionController.cs

--- APIGatewayS1/Controllers/TransactionController.cs
+++ APIGatewayS1/Controllers/TransactionController.cs
@@ -1,6 +1,18 @@
-﻿namespace APIGatewayS1.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIGateway.Controllers
 {
-    public class TransactionController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
     {
+        // Aquí se pueden definir endpoints para realizar solicitudes
+        // a otros microservicios si es necesario
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Transaction Service Gateway");
+        }
     }
 }
